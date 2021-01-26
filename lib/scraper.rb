5d94651fab75aa100b7cf66036d6e3bae7aa418e
Jacob Lepler <lepler.jacob@gmail.com>
2021-01-26 01:23:25 -0500

    working on scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,12 +3,22 @@ require 'httparty'
 require 'byebug'
 
 # Jacob was here on Jason's branch
+module Scraper
 
-def scaper
-    url = "https://blockwork.cc"
-    unpatsed_page = HTTParty
-    parsed_page = Nokogiri::HTML(unparsed)
-    byebug
+    module ScraperJobSearch
+
+        def scraper(city, state)
+            url = "https://www.ziprecruiter.com/candidate/search?radius=25&search=software+engineer+ruby&location=#{city}%2C+#{state}"
+            unparsed = HTTParty.get(url)
+            parsed_page = Nokogiri::HTML(unparsed)
+            jobs = parsed_page.css("h2>span[class='just_job_title']")
+            # byebug
+        end
+
+    end
+
 end
 
-scaper
+include Scraper::ScraperJobSearch
+
+# scraper
