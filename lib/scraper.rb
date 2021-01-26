d84cea5552985c65de1ec1a9b8a01054b95c9f81
Jacob Lepler <lepler.jacob@gmail.com>
2021-01-26 14:38:18 -0500

    removed dot causing error to url

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,8 +11,8 @@ module Scraper
             url = "https://www.ziprecruiter.com/candidate/search?radius=25&search=software+engineer+ruby&location=#{city}%2C+#{state}"
             unparsed = HTTParty.get(url)
             parsed_page = Nokogiri::HTML(unparsed)
-            jobs = parsed_page.css("h2>span[class='just_job_title']").
-            # byebug
+            jobs = parsed_page.css("h2>span[class='just_job_title']")
+            byebug
         end
 
     end
@@ -20,5 +20,5 @@ module Scraper
 end
 
 include Scraper::ScraperJobSearch
-
-# scraper
+# ("h2>span[class='just_job_title']")
+scraper("Atlanta","GA")
