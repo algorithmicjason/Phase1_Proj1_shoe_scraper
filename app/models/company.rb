a340c307e373dbb03a3a3e523c99f5a4e9787224
Jacob Lepler <lepler.jacob@gmail.com>
2021-01-27 23:29:41 -0500

    added associations to all models first attempt

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,5 @@
 class Company < ApplicationRecord
+    has_many :candidate, through: :contract
+	has_many :contract, through: :recruiter
+	has_many :listing 
 end
--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,2 +1,5 @@
 class Contract < ApplicationRecord
+    belongs_to :recruiter
+	belongs_to :company
+	belongs_to :candidate
 end
